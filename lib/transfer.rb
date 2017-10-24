872b573e9abfaece4cfe286a8a506d7d74b6abfc
Ben Blackman <bblkmn5@gmail.com>
2017-10-24 03:22:25 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -10,22 +10,29 @@ class Transfer
     @amount = amount
   end
 
+#can check that both accounts are valid
   def valid?
+#calls on the sender and receiver's #valid? methods
     sender.valid? && receiver.valid?
   end
 
+#can execute successful transaction btwn two accounts
   def execute_transaction
+#each transfer can only happen once
     if valid? && sender.balance > amount && self.status == "pending"
       receiver.balance += amount
       sender.balance -= amount
       self.status = "complete"
     else
+#rejects a transfer if the sender doesn
       self.status = "rejected"
       "Transaction rejected. Please check your account balance."
     end
   end
 
+#can reverse a transfer between 2 accounts
   def reverse_transfer
+#can only reverse executed accounts
     if valid? && receiver.balance > amount && self.status == "complete"
       receiver.balance -= amount
       sender.balance += amount
