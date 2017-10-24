3aa3f264f431f0c28dc4994577a1bb820fe8e2a8
Ben Blackman <bblkmn5@gmail.com>
2017-10-24 02:30:20 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,8 +1,11 @@
 class BankAccount
 
   attr_reader :name
-  
-  def initialize(name)
+
+  attr_accessor :balance
+
+  def initialize(name, balance = 1000)
     @name = name
+    @balance = balance
   end
 end
